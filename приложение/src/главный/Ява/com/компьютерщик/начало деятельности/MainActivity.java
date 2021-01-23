540ebb8b76283ef5bf5e506cd302f443e6953445
Aidar4ik <49788687+Aidar4ik@users.noreply.github.com>
2021-01-23 09:07:05 +0600

    Update and rename app/src/main/java/com/geektech/startactivityhomework/MainActivity.java to приложение/src/главный/Ява/com/компьютерщик/начало деятельности/MainActivity.java

--- приложение/src/главный/Ява/com/компьютерщик/начало деятельности/MainActivity.java
+++ приложение/src/главный/Ява/com/компьютерщик/начало деятельности/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
                     sum = (a + b) / c;
                     s= String.valueOf(sum);
                 }
-                textVie.setText(s);
             }
         });
     }
